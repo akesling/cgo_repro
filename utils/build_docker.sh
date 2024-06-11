b2a6c2345178f7be798439c538a715fb34022d46
A K <alex.kesling@gmail.com>
2024-06-11 10:53:48 -0400

    Looks like we needed a cross-compiling toolchain and an extra features flag

--- utils/build_docker.sh
+++ utils/build_docker.sh
@@ -9,15 +9,16 @@ _build_staging_dir="${_repo_root}/build/docker/"
 _dockerfile="${_repo_root}/Dockerfile"
 
 _build_target="//repro:linux_tarball"
+_build_flags="--compilation_mode=opt --features=fully_static_link"
 
 echo "Creating build staging directory ${_build_staging_dir}"
 mkdir -p "${_build_staging_dir}"
 
 echo "Building tarball"
 # Build straight to print status / progress
-bazel build -c opt "${_build_target}"
+bazel build  ${_build_flags} "${_build_target}"
 # Run again to capture file name
-_release_tarball="$(bazel build -c opt "${_build_target}" 2>&1 | grep "/tar.tar" | tr -d ' ')"
+_release_tarball="$(bazel build ${_build_flags} "${_build_target}" 2>&1 | grep "/tar.tar" | tr -d ' ')"
 
 echo "Copying dependencies to build tree for use by docker"
 rsync -a --delete "${_repo_root}/${_release_tarball}" "${_build_staging_dir}/repro.tar"
